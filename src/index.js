b483852c160d54ef412ef04cdb6bd535d89a2439
Paulo Choque <Paulo.Choque@fundacion-jala.org>
2022-01-24 17:33:35 -0400

    login component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+// import App from './components/App';
+import Loggin from './pages/loggin/Loggin';
 import './index.css';
 
-render(<App />, document.getElementById('app'));
+render(<Loggin />, document.getElementById('app'));
--- src/pages/loggin/Loggin.js
+++ src/pages/loggin/Loggin.js
@@ -1,13 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './StyleLogin.css';
 
 function Loggin() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [condition, setCondition] = useState(true);
+  function showHide() {
+    setCondition(!condition);
+    if (condition == false) {
+      cancel();
+    }
+  }
+  function handleSubmit() {
+    console.log(username);
+    console.log(password);
+  }
+  function cancel() {
+    setUsername('');
+    setPassword('');
+  }
+  const form = (
+    <>
+      <div className='loginForm'>
+        <form onSubmit={handleSubmit} className='form'>
+          <p><b>Email address</b></p>
+          <input className='inputText'
+            type="text"
+            value={username}
+            placeholder="email@example.com"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+          <p><b>Password</b></p>
+          <input className='inputText'
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </form>
+        <div className='buttons'>
+          <button onClick={cancel} className="button cancel">
+            Cancel
+          </button>
+          <button onClick={handleSubmit} className="button signin">
+            Sign in
+          </button>
+        </div>
+      </div>
+    </>
+  );
+
   return (
     <div className="loggin">
-      <h1>Loggin</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
-        quisquam.
-      </p>
+      <button onClick={showHide}>Login</button>
+      {condition ? form : null}
     </div>
   );
 }
