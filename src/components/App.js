cfb689808ffec61800938c56bb71f5bfaead4273
AlexGonzalesMeneses <blasterjerlene@gmail.com>
2022-01-24 16:37:51 -0400

    added header, footer and about page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { initialize, pageview } from 'react-ga';
+import { navigationMock, profileMock, socialMock } from '../../tools/MockData';
+import About from '../pages/about/About';
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
 
 function App() {
-  return <h1>Hello World</h1>;
+  initialize('UA-170984824-1');
+  pageview(window.location.pathname);
+
+  return (
+    <div className="App">
+      <Header {...navigationMock} />
+      <About {...profileMock} />
+      <Footer {...socialMock} />
+    </div>
+  );
 }
 
 export default App;
